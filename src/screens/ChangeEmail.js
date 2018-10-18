1f05b5ea2ce0ce95194bcb860504b700083c6cc1
Ulisses Ferreira <ulissesvf@gmail.com>
2018-10-18 16:58:25 +0100

    feat: change email after token link clicked component

---
+++ src/screens/ChangeEmail.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import * as Api from '../utils/Api'
+
+export default class ChangeEmail extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      token : window.location.href.split('?token=')[1],
+      email : ''
+    }
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({
+      email : event.target.value
+    })
+  }
+
+  changeEmail = () => {
+    Api.changeEmailWithToken(this.state.token, this.state.email).then(res => {
+      if (res.ok === 1) {
+        alert('Email changed')
+      } else {
+        alert('Something went wrong')
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Welcome to the email change!
+        <input type='email' onChange={this.handleEmailChange}/>
+        <button onClick={this.changeEmail}>Change Email</button>
+      </div>
+    );
+  }
+}
