33335058f047cb88915f44492dec7d765c32e983
Ulisses Ferreira <ulissesvf@gmail.com>
2018-10-19 17:23:36 +0100

    feat: abstract our WHOLE api into a lib that can be used on the front end

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,12 +1,12 @@
 export function login (email, password) {
-  let loginInfo = {
+  let fetchBody = {
     email: email,
     password: password
   }
   let fetchData = {
     method: 'POST',
     mode: 'cors',
-    body: JSON.stringify(loginInfo),
+    body: JSON.stringify(fetchBody),
     headers: {
       'Content-Type': 'application/json'
     }
@@ -14,45 +14,21 @@ export function login (email, password) {
   return fetch('http://localhost/auth/login', fetchData).then(res => res.json())
 }
 
-export function verify () {
-  let fetchData = {
-    method: 'GET',
-    mode: 'cors',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    }
+export function register (email, password, name) {
+  let fetchBody = {
+    email: email,
+    password: password,
+    name: name
   }
-  return fetch('http://localhost/profile/me', fetchData).then(res => res.json())
-}
-
-export function update (updateInfo) {
-  updateInfo['suggestions'] = undefined
-  updateInfo['tags'] = JSON.stringify(updateInfo['tags'])
   let fetchData = {
-    method: 'PATCH',
+    method: 'POST',
     mode: 'cors',
-    credentials: 'include',
-    body: JSON.stringify(updateInfo),
+    body: JSON.stringify(fetchBody),
     headers: {
       'Content-Type': 'application/json'
     }
   }
-  return fetch('http://localhost/profile/me', fetchData).then(res => res.json())
-}
-
-export function uploadPhoto () {
-  var input = document.querySelector('input[type="file"]')
-
-  var data = new FormData()
-  data.append('file', input.files[0])
-
-  return fetch('http://localhost/profile/image', {
-    method: 'POST',
-    credentials: 'include',
-    mode: 'cors',
-    body: data
-  }).then(res => res.json())
+  return fetch('http://localhost/auth/register', fetchData).then(res => res.json())
 }
 
 export function resetPassword(email) {
@@ -85,7 +61,7 @@ export function changeEmail(email, token) {
   return fetch('http://localhost/auth/changemyemail', fetchData).then(res => res.json())
 }
 
-export function resetPasswordWithToken (token, password) {
+export function resetPasswordWithToken(token, password) {
   let resetInfo = {
     token: token,
     password: password
@@ -101,7 +77,7 @@ export function resetPasswordWithToken (token, password) {
   return fetch('http://localhost/auth/forgotmypassword', fetchData).then(res => res.json())
 }
 
-export function changeEmailWithToken (token, email) {
+export function changeEmailWithToken(token, email) {
   let resetInfo = {
     email: email,
     token: token
@@ -115,4 +91,162 @@ export function changeEmailWithToken (token, email) {
     }
   }
   return fetch('http://localhost/auth/changemyemail', fetchData).then(res => res.json())
+}
+
+export function verifyKeycode (keycode) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch(`http://localhost/mentor/verify?keycode=${keycode}`, fetchData).then(res => res.json())
+}
+
+export function verifyUniqueId(uniqueid) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch(`http://localhost/mentor/verify?uniqueid=${uniqueid}`, fetchData).then(res => res.json())
+}
+
+export function getRandomMentors() {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch('http://localhost/mentor/random', fetchData).then(res => res.json())
+}
+
+export function getMentorInfo(keycode) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch(`http://localhost/mentor/${keycode}`, fetchData).then(res => res.json())
+}
+
+export function createMeetup (location, start, mentorKeycode) {
+  let fetchBody = {
+    location: location,
+    start: start,
+    mentorKeycode
+  }
+  let fetchData = {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
+    body: JSON.stringify(fetchBody),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost/meetup/', fetchData).then(res => res.json())
+}
+
+export function getMeetups() {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost/meetup', fetchData).then(res => res.json())
+}
+
+export function confirmMeetup(meetupId) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch(`http://localhost/meetup/confirm?meetup=${meetupId}`, fetchData).then(res => res.json())
+}
+
+export function getUserInfo () {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch('http://localhost/profile/me', fetchData).then(res => res.json())
+}
+
+export function updateUserInfo (updateInfo) {
+  let fetchData = {
+    method: 'PATCH',
+    mode: 'cors',
+    credentials: 'include',
+    body: JSON.stringify(updateInfo),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost/profile/me', fetchData).then(res => res.json())
+}
+
+export function uploadPhoto () {
+  var input = document.querySelector('input[type="file"]')
+
+  var data = new FormData()
+  data.append('file', input.files[0])
+
+  return fetch('http://localhost/profile/image', {
+    method: 'POST',
+    credentials: 'include',
+    mode: 'cors',
+    body: data
+  }).then(res => res.json())
+}
+
+export function searchQuick(query) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch(`http://localhost/search/quick?term=${query}`, fetchData).then(res => res.json())
+}
+
+export function searchFull(query) {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch(`http://localhost/search/full?term=${query}`, fetchData).then(res => res.json())
+}
+
+export function getSearchTags() {
+  let fetchData = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  return fetch('http://localhost/search/tags', fetchData).then(res => res.json())
 }
