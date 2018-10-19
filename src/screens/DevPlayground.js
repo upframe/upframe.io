b4817debc28e1e365c601069074784f123ebf4fe
Ulisses Ferreira <ulissesvf@gmail.com>
2018-10-19 17:23:56 +0100

    feat: add a testing page so that we can play around with the API lib

---
+++ src/screens/DevPlayground.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import * as Api from '../utils/Api'
+
+export default class DevPLayground extends Component {
+
+  render() {
+    return (
+      <div>
+        Here we can test dev stuff
+        <button>Login</button>
+        <button>Register</button>
+        <button>ForgotPass</button>
+        <button>ChangeMyEmail</button>
+        <button>ForgotPassTokenFinal</button>
+        <button>ChangeMyEmailTokenFInal</button>
+        <button>MentorVerify</button>
+        <button>MentorVerify2</button>
+        <button>GetMentorRandom</button>
+        <button>GetMentorKeycode</button>
+        <button>POST meetup</button>
+        <button>GET meetup</button>
+        <button>GET meetup confirm</button>
+        <button>GET profile me</button>
+        <button>Patch profile me</button>
+        <button>Post profile image</button>
+        <button>GET seach quick</button>
+        <button>GET seach full</button>
+        <button>GET seach tags</button>
+      </div>
+    );
+  }
+}
