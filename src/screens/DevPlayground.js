3b6b1671aeb194ed1a29b2e1962f4d79b2caebc4
Ulisses Ferreira <ulissesvf@gmail.com>
2018-10-19 17:30:21 +0100

    feat: add example

--- src/screens/DevPlayground.js
+++ src/screens/DevPlayground.js
@@ -4,11 +4,18 @@ import * as Api from '../utils/Api'
 
 export default class DevPLayground extends Component {
 
+  login = () => {
+    Api.login('hello@example.com', 'hello').then(res => {
+      console.log(res)
+      //Do stuff
+    })
+  }
+
   render() {
     return (
       <div>
         Here we can test dev stuff
-        <button>Login</button>
+        <button onClick={this.login}>Login</button>
         <button>Register</button>
         <button>ForgotPass</button>
         <button>ChangeMyEmail</button>
