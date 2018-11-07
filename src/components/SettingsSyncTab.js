3db6359c03fbcf2b878698b97e81bf13b184cfba
Ulisses Ferreira <ulissesvf@gmail.com>
2018-11-07 18:28:21 +0000

    feat: add Google Login Button

--- src/components/SettingsSyncTab.js
+++ src/components/SettingsSyncTab.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 export default class SettingsSyncTab extends Component {
+
+  googleResponse = (e) => {
+    console.log(e)
+  }
+
   render() {
     return (
-      <h1>Sou uma SYnc tab</h1>
+      <GoogleLogin
+        clientId="821697749752-k7h981c73hrji0k96235q2cblsjpkm7t.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.googleResponse}
+        onFailure={this.googleResponse}
+      />
     );
   }
 }
