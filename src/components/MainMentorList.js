f3685bbf9af9a7b2bd2edbe12c71a0400d0411f1
Ulisses Ferreira <ulissesvf@gmail.com>
2018-11-12 01:51:07 +0000

    feat: main page mentor list component

--- src/components/MainMentorList.js
+++ src/components/MainMentorList.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import * as Api from '../utils/Api'
 
 export default class MainMentorList extends Component {
-  render() {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      mentors: []
+    }
+  }
+
+  componentDidMount () {
+    Api.getRandomMentors().then(res => {
+      this.setState({
+        mentors: res.mentor
+      })
+    })
+  }
+
+  mentorTagsToElement = (tags) => {
+    return tags.map(tag => {
+      return (
+        <li className='mentor-tags-list-element'>{tag}</li>
+      )
+    })
+  }
+
+  mentorToElement = (mentor) => {
     return (
-      <h1>Sou uma Mentor List</h1>
-    );
+      <Link to={'/people/' + mentor.keycode}>
+        <div className='mentor-cards'>
+          <img alt={mentor.name} src={mentor.profilePic} />
+          <p>{mentor.name}</p>
+          <p>{mentor.role} @  
+            <Link to={'/companies/' + mentor.company}>{mentor.company}</Link>
+          </p>
+          <p>{mentor.bio}</p>
+          <p>{mentor.tags}</p>
+          {/* <ul>
+            {this.mentorTagsToElement(mentor.tags)}
+          </ul> */}
+          Fim
+        </div>
+      </Link>
+    )
+  }
+
+  render () { // For each mentor...
+    if (this.state.mentors !== []) {
+      return (
+        <div>
+          {this.state.mentors.map(mentor => {
+            return this.mentorToElement(mentor)
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
   }
 }
