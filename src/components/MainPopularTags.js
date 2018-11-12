5fbe737e36a76023fd5fc1321158ac514c691a62
Ulisses Ferreira <ulissesvf@gmail.com>
2018-11-12 02:38:38 +0000

    feat: main screen popular tags components

--- src/components/MainPopularTags.js
+++ src/components/MainPopularTags.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import * as Api from '../utils/Api'
 
-export default class MainPopularTags extends Component {
+export default class MainSearchBar extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      tags : []
+    }
+  }
+
+  componentDidMount() {
+    Api.getSearchTags().then(res => {
+      this.setState({
+        tags : res
+      })
+    })
+  }
+
   render() {
-    return (
-      <h1>Sou umas Popular Tags</h1>
-    );
+    if (this.state.tags !== []) {
+      return (
+        <div>
+          {this.state.tags.map(tag => {
+            return (<div><Link to={'/search/' + tag}>
+              {tag}
+            </Link></div>)
+          })}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
   }
 }
