d8e0c3c34da8af093ce200021dba72e94c2d0a66
Ulisses Ferreira <ulissesvf@gmail.com>
2018-12-09 17:18:49 +0000

    feat: search bar with quick suggestions (companies, people and expertise)

---
+++ src/screens/Expertise.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class Expertise extends Component {
+  //Someone accessed the website with a /people/* something.
+  //Hypothesis:
+  // /expertise-name - render resultado da pesquisa expertise
+  // /keycode - render profile
+  // /company - everyone who works at company
+  render() {
+    return (
+      <h1>Sou uma expertise</h1>
+    );
+  }
+} 
---
+++ src/screens/Meetup.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class Meetup extends Component {
+  render() {
+    return (
+      <h1>Sou um Meetup</h1>
+    );
+  }
+} 
