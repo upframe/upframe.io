6e740afc714e5ee08e28e55058d683e53389f376
Ulisses Ferreira <ulissesvf@gmail.com>
2019-01-16 23:43:16 +0000

    fix: 'Grab a coffe' typo

--- src/components/MentorMeetupPopup.js
+++ src/components/MentorMeetupPopup.js
@@ -92,7 +92,7 @@ export default class MentorMeetupPopup extends Component {
               <div className='meetup-option' onClick={this.grabCoffee}>
                 <img src='/media/coffee.svg' alt='Coffee cup'></img>
                 <div>
-                  <h1 className='meetup-option-title font-weight-normal'>Grab a coffe</h1>
+                  <h1 className='meetup-option-title font-weight-normal'>Grab a coffEe</h1>
                   <p>Pick from a selection of Malik's favourite cafés and local coworking spaces.</p>
                 </div>
               </div>
