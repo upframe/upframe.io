35f1d91c5a216c81b169b55c3fc9084e4b3562b2
Ulisses Ferreira <ulissesvf@gmail.com>
2019-01-22 16:10:57 +0000

    fix: replace old location icon with new one

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'font-awesome/css/font-awesome.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
