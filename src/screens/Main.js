0182e948a71adcf5d20155ea439d4f5c046afac4
Ulisses Ferreira <ulissesvf@gmail.com>
2019-02-01 20:12:40 +0000

    chore: linting

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Component } from 'react';
+// import { Redirect } from 'react-router-dom'
 
 // import MainMentorList from '../components/MainMentorList'
 // import MainPopularTags from '../components/MainPopularTags'
 // import MainSearchBar from '../components/MainSearchBar'
 
 export default class Main extends Component {
+
   render() {
-    return (
-      <Redirect to='/malikpiara' />
-      // <main id="home">
-      //   <div className="container grid">
-      //     <MainSearchBar />
-      //     <MainPopularTags />
-      //     <MainMentorList />
-      //   </div>
-      // </main>
-    );
+    window.location = 'https://facebook.com/upframe'
+    return (null)
+    // return (
+    //   <Redirect to='https://facebook.com/upframe' />
+    //   // <main id="home">
+    //   //   <div className="container grid">
+    //   //     <MainSearchBar />
+    //   //     <MainPopularTags />
+    //   //     <MainMentorList />
+    //   //   </div>
+    //   // </main>
+    // );
   }
 }
