45fe41d429315fe4aa17d8c4add0e019512ee785
Fábio Ferreira <ffcfpten@gmail.com>
2019-02-03 15:44:27 +0000

    chores: remove calendar list modifications

--- src/components/SettingsAccountTab.js
+++ src/components/SettingsAccountTab.js
@@ -26,12 +26,12 @@ export default class SettingsAccountTab extends Component {
   render() {
     return (
       <div id='settings-accounttab' className='tab center'>
-        <h1>Email</h1>
+        <h1 className='font-weight-normal'>Email</h1>
         <p>
           Your email address is <a href={'mailto:' + this.context.user.email}>{this.context.user.email}</a>. This information will not be publicly displayed
         </p>
         <button onClick={this.changeEmail}>Change Email</button>
-        <h1>Password</h1>
+        <h1 className='font-weight-normal'>Password</h1>
         <p>Set a unique password to protect your account</p>
         <button onClick={this.changePassword}>Change Password</button>
       </div>
