6754e7e0924bd2e489eca0969aa3e5991154cae4
Ulisses Ferreira <ulissesvf@gmail.com>
2019-02-04 18:19:12 +0000

    feat: Mixpanel integration (test)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('993a3d7a78434079b7a9bec245dbaec2');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
