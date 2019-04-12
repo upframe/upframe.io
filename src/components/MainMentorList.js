6c39b60755f446706573c4e22d9e1011c58a663a
Fábio Ferreira <ffcfpten@gmail.com>
2019-04-12 21:51:16 +0100

    fix: mobile mentor list

--- src/components/MainMentorList.js
+++ src/components/MainMentorList.js
@@ -6,7 +6,7 @@ export default class MainMentorList extends PureComponent {
   render() {
     if (this.props.mentors !== [] && this.props.mentors !== undefined) {
       return (
-        <div className="mentor-list">
+        <div className="mentor-list flex flex-column">
           {this.props.mentors.map((mentor, index) => {
             return (
               <MentorCard key={index} mentorInfo={mentor} animation='fade-up' index={index} />
