df400e05ca9f47f472c54d0245076957bf0a14b6
Fábio Ferreira <ffcfpten@gmail.com>
2019-04-28 04:41:51 +0100

    feat: animated top notification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,26 @@ import { Link } from 'react-router-dom'
 
 import AppContext from './AppContext'
 
-import { getCookies } from '../utils/Cookies'
+import { docCookies } from '../utils/Cookies'
 
-// import emojis
-// import '../emojis.css'
+// import emojis and icons
+import '../icons.css'
 
 export default class Navbar extends Component {
   static contextType = AppContext
+
+  constructor(props) {
+    super(props);
+
+    let firstVisit = docCookies.getItem('firstVisit')
+    if (firstVisit && Number(firstVisit) === 0) firstVisit = false;
+    else firstVisit = true;
+
+    this.state = {
+      firstVisit,
+      cookieUpdated: false,
+    }
+  }
 
   openDropdown = () => {
     let dropdown = document.querySelector('nav div.dropdown')
@@ -28,15 +41,27 @@ export default class Navbar extends Component {
     this.context.logout()
   }
 
-  render() {
-    let firstVisit = (getCookies())['firstVisit']
+  // Redefine cookie to hide notification and
+  hideNotification = () => {
+    docCookies.setItem('firstVisit', 0, Infinity, '/', '', false)
     
+    this.setState({ cookieUpdated: true })
+    setTimeout(() => {
+      document.querySelector('header').id = ''
+    }, 500)
+  }
+
+  render() {
     return (
-      <header id={ firstVisit !== false ? 'with-notification' : null }>
-        { firstVisit !== false ?
+      <header id={ this.state.firstVisit ? 'with-notification' : null }
+        className={this.state.cookieUpdated ? 'hide' : null}>
+        { this.state.firstVisit === true ?
           <div id='first-visit-notification' className='flex alignitems-center justifycontent-center'>
-            <span className='cupcake'></span>Ahoy! Upframe is currently invite-only. Drop us some lines at
-            team@upframe.io and we’ll keep you posted.</div>
+            {/* <span className='cupcake'></span> */}
+            <p>Ahoy! Upframe is currently invite-only. Drop us some lines at
+            team@upframe.io and we’ll keep you posted.</p>
+            <span className='arrow' onClick={this.hideNotification}></span>  
+          </div>
           : null
         }
 
@@ -56,12 +81,11 @@ export default class Navbar extends Component {
                 </ul>
               </div>
             :
-              null
-              /*<div className='flex flex-column alignitems-center'>
+              <div className='flex flex-column alignitems-center'>
                 <ul>
                   {window.location.pathname === '/login' ? null : <li><Link to='/login'>Login</Link></li> }
                 </ul>
-              </div>*/
+              </div>
             }
           </div>
         </nav>
