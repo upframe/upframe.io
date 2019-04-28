96ae3a0c09ec43f30c75be46bf0739814d96226a
Fábio Ferreira <ffcfpten@gmail.com>
2019-04-28 05:21:35 +0100

    styles: categories list items

--- src/components/MainCategories/index.js
+++ src/components/MainCategories/index.js
@@ -9,7 +9,17 @@ export default class MainCategories extends Component {
         <h1 className='font-150 fontweight-medium'>Top Categories</h1>
         <p>How can we help? Start by picking one of our main categories.</p>
         <div className='grid'>
-          <div className='category-item'></div>
+          <div className='category-item flex alignitems-center'>
+            <h1 className='white-text'>Business</h1>
+          </div>
+          
+          <div className='category-item'>
+            <h1>Design</h1>
+          </div>
+
+          <div className='category-item'>
+            <h1>Technology</h1>
+          </div>
         </div>
       </div>
     )
