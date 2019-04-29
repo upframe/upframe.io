d0030e5518c69fb40979f982259fcc194d699b43
Fábio Ferreira <ffcfpten@gmail.com>
2019-04-29 20:58:22 +0100

    styles: categories items animations

--- src/components/MainCategories/index.js
+++ src/components/MainCategories/index.js
@@ -11,21 +11,24 @@ const MainCategories = props => {
       <i className="em em-star2"></i>Top Categories</h1>
       <p>How can we help? Start by picking one of our main categories.</p>
       <ul className='list-reset grid'>
-        <Link to='/business'>
+        <Link to='/business' data-aos='fade-up'
+          data-aos-delay='0' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Business' title='Business' id="category-image"></div>
             <h1>Business</h1>
           </li>
         </Link>
         
-        <Link to='/design'>
+        <Link to='/design' data-aos='fade-up'
+          data-aos-delay='0' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Design' title='Design' id="category-image"></div>
             <h1>Design</h1>
           </li>
         </Link>
 
-        <Link to='/technology'>
+        <Link to='/technology' data-aos='fade-up'
+          data-aos-delay='0' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Technology' title='Technology' id="category-image"></div>
             <h1>Technology</h1>
