6cbca65d8b88752518024d78bd5ff859823c1356
Fábio Ferreira <ffcfpten@gmail.com>
2019-04-29 21:15:04 +0100

    fix: categories section animations

--- src/components/MainCategories/index.js
+++ src/components/MainCategories/index.js
@@ -7,28 +7,28 @@ const MainCategories = props => {
 
   return (
     <div id='categories-list'>
-      <h1 className='font-150 fontweight-medium'>
+      <h1 className='font-150 fontweight-medium' data-aos='fade-left'
+        data-aos-delay='0' data-aos-offset='0'>
       <i className="em em-star2"></i>Top Categories</h1>
-      <p>How can we help? Start by picking one of our main categories.</p>
+      <p data-aos='fade-left' data-aos-delay='200' data-aos-offset='0'>
+        How can we help? Start by picking one of our main categories.
+      </p>
       <ul className='list-reset grid'>
-        <Link to='/business' data-aos='fade-up'
-          data-aos-delay='0' data-aos-offset='0'>
+        <Link to='/business' data-aos='fade-left' data-aos-delay='300' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Business' title='Business' id="category-image"></div>
             <h1>Business</h1>
           </li>
         </Link>
         
-        <Link to='/design' data-aos='fade-up'
-          data-aos-delay='0' data-aos-offset='0'>
+        <Link to='/design' data-aos='fade-left' data-aos-delay='400' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Design' title='Design' id="category-image"></div>
             <h1>Design</h1>
           </li>
         </Link>
 
-        <Link to='/technology' data-aos='fade-up'
-          data-aos-delay='0' data-aos-offset='0'>
+        <Link to='/technology' data-aos='fade-left' data-aos-delay='500' data-aos-offset='0'>
           <li className='category-item'>
             <div alt='Technology' title='Technology' id="category-image"></div>
             <h1>Technology</h1>
