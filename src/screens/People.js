f7d2e5281f5429d7f4a4bafd0233b8b85a007dc0
Fábio Ferreira <ffcfpten@gmail.com>
2019-08-03 13:21:06 +0200

    fix: social network mentor page description

--- src/screens/People.js
+++ src/screens/People.js
@@ -231,7 +231,7 @@ export default class People extends Component {
           <Helmet>
             <title>{this.state.mentor.name} | Upframe</title>
             <meta property="og:title" content={`${this.state.mentor.name} | Upframe`}></meta>
-            <meta property="og:description" content={`Setup a meetup with ${this.state.mentor.name}. ${this.state.mentor.bio.substr(0, 128)}...`}></meta>
+            <meta property="og:description" content={`Set up a meetup with ${this.state.mentor.name}. ${this.state.mentor.bio.substr(0, 128)}...`}></meta>
             <meta property="og:image" content={this.state.mentor.profilePic}></meta>
             <meta name="twitter:card" content="summary_large_image"></meta>
           </Helmet>
