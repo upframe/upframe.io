6e86c0cec3ae667cdfac99512aaf86baaa716452
MathisBullinger <mathis.bullinger@code.berlin>
2019-11-13 01:39:03 +0100

    refactor: remove unused service worker

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 import mixpanel from 'mixpanel-browser'
 
 mixpanel.init('993a3d7a78434079b7a9bec245dbaec2')
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
