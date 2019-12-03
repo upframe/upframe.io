4e517b8c0677bab9b902c1eafd59f0964ac17507
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-03 17:45:48 +0100

    feat: navigation highlight active tab

--- src/screens/Settings/Navigation.jsx
+++ src/screens/Settings/Navigation.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './navigation.module.scss'
 
+const tabs = [
+  { title: 'Public Profile', url: 'public' },
+  { title: 'Account Settings', url: 'account' },
+  { title: 'Notifications', url: 'notifications' },
+  { title: 'Calendar', url: 'sync' },
+]
+
 export default function Navigation() {
+  const current = window.location.href.split('/').pop()
+
   return (
     <nav className={styles.navigation}>
-      <Link className={styles.item} to="/settings/public">
-        Public Profile
-      </Link>
-      <Link
-        className={[styles.item, styles.active].join(' ')}
-        to="/settings/account"
-      >
-        Account Settings
-      </Link>
-      <Link className={styles.item} to="/settings/notifications">
-        Notifications
-      </Link>
-      <Link className={styles.item} to="/settings/sync">
-        Calendar
-      </Link>
+      {tabs.map(({ title, url }) => (
+        <Link
+          className={[
+            styles.item,
+            ...(current === url ? [styles.active] : []),
+          ].join(' ')}
+          to={`/settings/${url}`}
+          key={url}
+        >
+          {title}
+        </Link>
+      ))}
     </nav>
   )
 }
