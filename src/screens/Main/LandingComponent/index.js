bb5c2a0141f20246785d5a9b289f5affcfbe0752
Ulisses Ferreira <ulisses.ferreira@unbabel.com>
2019-12-05 22:21:12 +0000

    fix: Malik's stylistic changes to the main page

--- src/screens/Main/LandingComponent/index.js
+++ src/screens/Main/LandingComponent/index.js
@@ -8,7 +8,7 @@ const LandingComponent = () => {
   return (
     <div className="landing-component">
       <h1>The home for</h1>
-      <h1>First-time Founders.</h1>
+      <h1>first-time founders.</h1>
       <p>
         Upframe helps you find and connect with industry leaders that can give you startup
         advice and mentor you through the first steps of building a product.
