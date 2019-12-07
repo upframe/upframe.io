a69fe816ec7470711c1c2584be5532e8ebebfc76
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-07 17:28:58 +0100

    fix: change default notifications selection

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './checkbox.module.scss'
 
-export default function Checkbox({ onChange }) {
+export default function Checkbox({ onChange, checked }) {
   return (
     <input
       type="checkbox"
       className={styles.checkbox}
+      defaultChecked={checked}
       {...(onChange && { onChange })}
     />
   )
