bc877f3184206c2e2d48c373daeaddb237dfa6f4
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-07 19:58:28 +0100

    feat: change & redirect calendar url

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -6,7 +6,7 @@ import Account from './Account'
 import CalendarTab from './CalendarTab'
 import Navigation from './Navigation'
 import Notifications from './Notifications'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import styles from './Settings.module.scss'
 
 export default function Settings() {
@@ -32,7 +32,8 @@ export default function Settings() {
             <Route path="/settings/public" component={Profile} />
             <Route path="/settings/account" component={Account} />
             <Route path="/settings/notifications" component={Notifications} />
-            <Route path="/settings/sync" component={CalendarTab} />
+            <Route path="/settings/mycalendar" component={CalendarTab} />
+            <Redirect from="/settings/sync" to="/settings/mycalendar" />
           </Switch>
         </div>
       </main>
