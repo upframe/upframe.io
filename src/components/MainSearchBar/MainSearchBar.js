41561a0a8f7b55f9d41e763c003db2bdfa9dfce2
MathisBullinger <mathis.bullinger@code.berlin>
2019-12-14 16:05:22 +0100

    fix: don't refresh page on search reset

--- src/components/MainSearchBar/MainSearchBar.js
+++ src/components/MainSearchBar/MainSearchBar.js
@@ -1,51 +1,21 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-
-import AppContext from '../AppContext'
-
+import React from 'react'
 import styles from './MainSearchBar.module.scss'
 
-export default class MainSearchBar extends Component {
-  static contextType = AppContext
-  constructor(props) {
-    super(props)
-    this.state = {
-      searchQuery: '',
-    }
+export default function SearchBar({ onChange, onSubmit, searchQuery }) {
+  function handleSubmit(e) {
+    e.preventDefault()
+    onSubmit()
   }
 
-  handleChange = event => {
-    this.setState({
-      searchQuery: event.target.value,
-    })
-  }
-
-  handleKeyPress = event => {
-    if (event.key === 'Enter') {
-      this.context.setSearchQuery(this.state.searchQuery)
-      this.context.startSearchQuery(true)
-    }
-  }
-
-  RedirectToMain = () => {
-    if (this.context.isSearchQuery) {
-      return <Redirect to="/" />
-    }
-  }
-
-  render() {
-    return (
-      <div className={styles.SearchWrapper}>
-        <input
-          type="text"
-          className={styles.input}
-          placeholder="What are you looking for?"
-          onChange={this.handleChange}
-          value={this.state.searchQuery}
-          onKeyPress={this.handleKeyPress}
-        />
-        {this.RedirectToMain()}
-      </div>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={styles.search}
+        placeholder="What are you looking for?"
+        onChange={({ target }) => onChange(target.value)}
+        value={searchQuery}
+      />
+    </form>
+  )
 }
