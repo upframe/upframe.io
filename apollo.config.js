5551a377af9d2ad2cc9008f27ad4cf3e3d7d2d01
Mathis Bullinger <mathis@bullinger.dev>
2020-06-28 10:44:29 +0200

    fix: load schema tag from enviroment if available

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 
-const env = fs.existsSync('.env')
+const env = process.env.SCHEMA_TAG
+  ? process.env
+  : fs.existsSync('.env')
   ? Object.fromEntries(
       fs
         .readFileSync('.env', 'utf-8')
@@ -9,11 +11,15 @@ const env = fs.existsSync('.env')
     )
   : {}
 
+const service =
+  env.LOCAL_SCHEMA === 'true'
+    ? `upframe@local-${require('os').userInfo().username}`
+    : `upframe@${env.SCHEMA_TAG || 'beta'}`
+
+console.log(`load schema version ${service}`)
+
 module.exports = {
   client: {
-    service:
-      env.LOCAL_SCHEMA === 'true'
-        ? `upframe@local-${require('os').userInfo().username}`
-        : `upframe@${env.SCHEMA_TAG || 'beta'}`,
+    service,
   },
 }
