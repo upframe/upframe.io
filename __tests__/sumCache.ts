43963fbf149618e413c63f4a4a5f3b677e6ec500
Mathis Bullinger <mathis@bullinger.dev>
2020-07-07 21:36:46 +0200

    feat: sum cache utility

---
+++ __tests__/sumCache.ts
@@ -0,0 +1,99 @@
+import Cache from 'utils/sumCache'
+
+describe('Sum Cache', () => {
+  let gets = 0
+  const get = (i: number) => {
+    gets++
+    return i * 10 + 5
+  }
+  const cache = new Cache(get)
+
+  const testCacheNum = (n: number) =>
+    it(`number of caches: ${n}`, () =>
+      expect((cache as any).caches.length).toBe(n))
+  const testGets = (n: number) =>
+    it(`number of evalutations: ${n}`, () => expect(gets).toBe(n))
+  const testAt = (i: number, v: number) =>
+    it(`has value at ${i}: ${v}`, () => expect(cache.at(i)).toBe(v))
+
+  it('read from cache', () => expect(cache.at(1)).toBe(15))
+  testCacheNum(1)
+  testGets(1)
+
+  it('can return value again', () => expect(cache.at(1)).toBe(15))
+  testGets(1)
+  testCacheNum(1)
+
+  it('can return next index', () => expect(cache.at(2)).toBe(25))
+  testGets(2)
+  testCacheNum(1)
+
+  it('sum up cache', () => expect(cache.sum(1, 2)).toBe(40))
+
+  it('read outside existing cache', () => expect(cache.at(5)).toBe(55))
+  testCacheNum(2)
+  testGets(3)
+
+  it('can sum outside existing range', () => expect(cache.sum(5, 7)).toBe(195))
+  it('can sum over multiple caches', () => expect(cache.sum(1, 7)).toBe(315))
+  testCacheNum(1)
+  testGets(7)
+
+  it('can sum subsection', () => expect(cache.sum(3, 5)).toBe(135))
+
+  it('read previous adjacent index', () => expect(cache.at(0)).toBe(5))
+  testCacheNum(1)
+
+  it('sum next adjacent indexes', () => expect(cache.sum(8, 9)).toBe(180))
+  testCacheNum(1)
+  it('sum previous adjacent indexes', () => expect(cache.sum(-2, -1)).toBe(-20))
+  testCacheNum(1)
+
+  // adjacent read merge
+  testAt(11, 115)
+  testCacheNum(2)
+  testAt(10, 105)
+  testCacheNum(1)
+  testAt(-4, -35)
+  testCacheNum(2)
+  testAt(-3, -25)
+  testCacheNum(1)
+
+  // reading over multiple caches
+  testAt(13, 135)
+  testAt(15, 155)
+  testAt(17, 175)
+  testGets(19)
+  testCacheNum(4)
+  it('sum over multiple caches', () => expect(cache.sum(-3, 18)).toBe(1760))
+  testCacheNum(1)
+
+  it('has no partitions', () =>
+    expect((cache as any).caches[0].partitions.length).toBe(0))
+  it('create partition', () => expect(cache.sum(0, 100)).not.toBeNull())
+  it('has 1 partition', () =>
+    expect((cache as any).caches[0].partitions.length).toBe(1))
+  it('create partition', () => expect(cache.sum(-100, 0)).not.toBeNull())
+  it('has 2 partitions', () =>
+    expect((cache as any).caches[0].partitions.length).toBe(2))
+
+  const calcSum = (start: number, end: number): number =>
+    Array(end - start + 1)
+      .fill(0)
+      .map((_, i) => i + start)
+      .map(i => i * 10 + 5)
+      .reduce((a, c) => a + c)
+
+  const testSum = (start: number, end: number, v = calcSum(start, end)) =>
+    it(`sum of ${start} to ${end} is ${v}`, () =>
+      expect(cache.sum(start, end)).toBe(v))
+
+  testSum(0, 100)
+
+  const steps = [17, 35, 78, 102, 153, 217, 389]
+
+  for (const step of steps) {
+    for (let i = 0; i < Math.floor(1000 / step); i++)
+      testSum(i * step, (i + 1) * step)
+  }
+})
---
+++ src/utils/sumCache.ts
@@ -0,0 +1,204 @@
+interface CacheAccess {
+  at(i: number): number
+  sum(start: number, length: number): number
+}
+
+class SumCache implements CacheAccess {
+  private partitions: SumCache[] = []
+
+  constructor(
+    public offset: number,
+    private readonly getValue: (i: number) => number,
+    private values: number[] = [],
+    private partitionLength = 100,
+    private _sum: number = values?.reduce((a, c) => a + c, 0)
+  ) {
+    if (!partitionLength || !values.length) return
+    for (let i = 0; i < ((values.length / partitionLength) | 0); i++) {
+      this.partitions.push(
+        new SumCache(
+          i * partitionLength + this.offset,
+          SumCache.noEval,
+          values.slice(i * partitionLength, (i + 1) * partitionLength),
+          0
+        )
+      )
+    }
+  }
+
+  public get max() {
+    return this.offset + this.values.length - 1
+  }
+
+  public at(i: number): number {
+    this.checkInRange(i)
+    if (this.values.length > i - this.offset && i - this.offset >= 0)
+      return this.values[i - this.offset]
+    return this.write(i)
+  }
+
+  public sum(start: number, end: number): number {
+    this.checkInRange(start, end)
+
+    if (start === this.offset && end === this.max) return this._sum
+    if (end === start) return this.values[start - this.offset]
+
+    let p1 = this.partitions.length
+      ? (this.partitions.find(
+          part => part.offset <= start && part.max >= end
+        ) as SumCache)
+      : undefined
+
+    if (!p1 || this.partStart > end || this.partEnd < start)
+      return this.values
+        .slice(start - this.offset, end - this.offset + 1)
+        .reduce((a, c) => a + c)
+
+    let v = 0
+
+    for (let i = start - this.offset; i < p1.offset - this.offset; i++)
+      v += this.values[i]
+
+    for (let i = this.partitions.indexOf(p1); this.partitions[i]; i++) {
+      v += this.partitions[i].sum(
+        Math.max(p1.offset, start),
+        Math.min(end, p1.max)
+      )
+      if (this.partitions[i].max >= end) return v
+    }
+
+    return v
+  }
+
+  private write(i: number, v: number = this.getValue(i)): number {
+    this.checkInRange(i)
+    if (i - this.offset >= 0 && i - this.offset < this.values.length) {
+      this.values[i] = v
+      return v
+    }
+    let front = i - this.offset < 0
+    if (front) {
+      this.offset--
+      this.values.unshift(v)
+    } else {
+      this.values.push(v)
+    }
+    this._sum += v
+
+    if (!this.partitionLength) return v
+
+    if (
+      !front &&
+      this.values.length + this.offset - this.partEnd === this.partitionLength
+    )
+      this.partitions.push(
+        new SumCache(
+          this.partEnd + 1,
+          SumCache.noEval,
+          this.values.slice(this.partEnd - this.offset + 1),
+          0
+        )
+      )
+    else if (front && this.partStart - this.offset === this.partitionLength)
+      this.partitions.unshift(
+        new SumCache(
+          this.offset,
+          SumCache.noEval,
+          this.values.slice(0, this.partStart - this.offset),
+          0
+        )
+      )
+
+    return v
+  }
+
+  private get partStart() {
+    return this.partitions[0]?.offset ?? this.values.length + this.offset
+  }
+
+  private get partEnd() {
+    return this.partitions[this.partitions.length - 1]?.max ?? this.offset - 1
+  }
+
+  private static noEval(): never {
+    throw Error("partition shouldn't reevalutate value")
+  }
+
+  private checkInRange(...indexes: number[]): void {
+    for (let i of indexes)
+      if (i - this.offset > this.values.length || i < this.offset - 1)
+        throw Error(`can't access index ${i}, cache must be continuous`)
+  }
+
+  public static merge(a: SumCache, b: SumCache): SumCache {
+    return new SumCache(
+      a.offset,
+      a.getValue,
+      [...a.values, ...b.values],
+      a.partitionLength,
+      a._sum + b._sum
+    )
+  }
+}
+
+export default class CacheManager implements CacheAccess {
+  private caches: SumCache[] = []
+
+  constructor(private readonly getValue: (i: number) => number) {}
+
+  public at(i: number): number {
+    const { cache, isNew } = this.getCache(i)
+    const v = cache.at(i)
+    if (isNew) return v
+    const next = this.getNext(cache)
+    if (next && next.offset === i + 1) this.merge(cache, next)
+    return v
+  }
+
+  private getCache(offset: number): { cache: SumCache; isNew: boolean } {
+    let i = 0
+    let cache: SumCache | undefined
+    for (i; i < this.caches.length; i++) {
+      if (this.caches[i].offset > offset + 1) break
+      else if (this.caches[i].max >= offset - 1) {
+        cache = this.caches[i]
+        break
+      }
+    }
+    if (cache) return { cache, isNew: false }
+
+    if (i === this.caches.length - 1 && this.caches[i].max < i) i++
+    cache = new SumCache(offset, this.getValue)
+    this.caches = [...this.caches.slice(0, i), cache, ...this.caches.slice(i)]
+    return { cache, isNew: true }
+  }
+
+  public sum(start: number, end = start): number {
+    if (end < start || !Number.isInteger(end))
+      throw Error('end must be integer >= start')
+
+    const step = (cache: SumCache): SumCache => {
+      const next = this.getNext(cache)
+      if (cache.max >= end || (next?.offset ?? Infinity) > end + 1) {
+        for (let i = cache.max + 1; i <= end; i++) cache.at(i)
+        return cache
+      }
+      for (let i = cache.max + 1; i < next?.offset; i++) cache.at(i)
+      return this.merge(cache, next)
+    }
+
+    let { cache } = this.getCache(start)
+    while (cache.max < end) cache = step(cache)
+    return cache.sum(start, end)
+  }
+
+  private merge(a: SumCache, b: SumCache): SumCache {
+    const merged = SumCache.merge(a, b)
+    this.caches.splice(this.caches.indexOf(a), 2, merged)
+    return merged
+  }
+
+  private getNext(cache: SumCache): SumCache {
+    return this.caches[this.caches.indexOf(cache) + 1]
+  }
+}
