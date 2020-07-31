4554e0b61f1d4d3c7c5b3d8d049641439438588e
Mathis Bullinger <mathis@bullinger.dev>
2020-07-31 22:41:50 +0200

    feat: update local time every full minute

--- src/screens/messages/Participant.tsx
+++ src/screens/messages/Participant.tsx
@@ -8,31 +8,8 @@ export default function Participants({
 }: {
   user: ChatParticipants['users'][number]
 }) {
-  const [time, setTime] = useState('')
-  const [tzKnown, setTzKnown] = useState(false)
-
   const utcOffset = user.timezone?.utcOffset
-  useEffect(() => {
-    let date = new Date()
-    if (utcOffset) {
-      const dUTC = date.getTimezoneOffset() - -utcOffset
-      date = new Date(date.getTime() + 1000 * 60 * dUTC)
-      setTzKnown(true)
-    }
-    let suffix = 'AM'
-    let hour = date.getHours()
-    if (hour >= 12) {
-      if (hour >= 13) hour -= 12
-      suffix = 'PM'
-    }
-    setTime(
-      `${date.toLocaleString('en-US', {
-        month: 'long',
-      })} ${date.getDate()}, ${hour}:${`0${date.getMinutes()}`.substr(
-        -2
-      )} ${suffix}`
-    )
-  }, [utcOffset])
+  const time = useLocalTime(utcOffset)
 
   return (
     <S.Participant>
@@ -42,13 +19,49 @@ export default function Participants({
         <S.Secondary>
           <S.Profile to={`/${user.handle}`}>View Profile</S.Profile> |{' '}
           <S.Time title={user.timezone?.informal?.current?.name ?? ''}>
-            {tzKnown ? 'Local Time ' : ''}
+            {typeof utcOffset === 'number' ? 'Local Time ' : ''}
             {time}
           </S.Time>
         </S.Secondary>
       </S.Info>
     </S.Participant>
   )
+}
+
+function useLocalTime(utcOffset = -new Date().getTimezoneOffset()) {
+  const [time, setTime] = useState('')
+
+  useEffect(() => {
+    let tId: number
+
+    function update() {
+      let date = new Date()
+      if (utcOffset) {
+        const dUTC = date.getTimezoneOffset() - -utcOffset
+        date = new Date(date.getTime() + 1000 * 60 * dUTC)
+      }
+      let suffix = 'AM'
+      let hour = date.getHours()
+      if (hour >= 12) {
+        if (hour >= 13) hour -= 12
+        suffix = 'PM'
+      }
+      setTime(
+        `${date.toLocaleString('en-US', {
+          month: 'long',
+        })} ${date.getDate()}, ${hour}:${`0${date.getMinutes()}`.substr(
+          -2
+        )} ${suffix}`
+      )
+
+      tId = setTimeout(update, (60 - new Date().getSeconds()) * 1000 + 100)
+    }
+    update()
+
+    return () => clearTimeout(tId)
+  }, [utcOffset])
+
+  return time
 }
 
 const picSize = '2.5rem'
