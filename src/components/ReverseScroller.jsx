7111d5d16c615cfbe88e26bb272029faad03e64e
Mathis Bullinger <mathis@bullinger.dev>
2020-08-13 07:43:39 +0200

    fix: firefox scrollable conversation preview

--- src/components/ReverseScroller.jsx
+++ src/components/ReverseScroller.jsx
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 
 function ReverseScroller({ children }) {
   return (
-    <S.Scroller>
+    <S.Scroller
+      {...(navigator.userAgent.toLowerCase().includes('firefox') && {
+        'data-browser': 'firefox',
+      })}
+    >
       {Array.isArray(children) ? [...children].reverse() : children}
     </S.Scroller>
   )
@@ -13,6 +17,23 @@ const S = {
   Scroller: styled.div`
     display: flex;
     flex-direction: column-reverse;
+
+    /* patch for firefox reverse flex scroll bug https://bugzilla.mozilla.org/show_bug.cgi?id=1042151
+    ...that still hasn't been fixed after 6 years */
+    &[data-browser='firefox'] {
+      flex-direction: column;
+      transform: rotate(180deg);
+      direction: rtl;
+
+      & > * {
+        transform: rotate(180deg);
+      }
+
+      * {
+        text-align: left;
+        flex-direction: row-reverse;
+      }
+    }
   `,
 }
 
