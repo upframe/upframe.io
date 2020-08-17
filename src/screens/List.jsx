540386a8bd73a45786344d8be0631cc07b9c6702
Mathis Bullinger <mathis@bullinger.dev>
2020-08-17 09:54:39 +0200

    fix: search result page closes #312

--- src/screens/List.jsx
+++ src/screens/List.jsx
@@ -27,12 +27,14 @@ export default function List({ match }) {
     type === 'list' ? data.list : type === 'tag' ? data.tag : data.search
 
   if (loading) return <Spinner centered />
-  if (list && name.replace(/_/g) !== list.name.replace(/\s/g))
+  if (type === 'list' && name.replace(/_/g) !== list.name.replace(/\s/g))
     return <Redirect replace to={list.name.replace(/\s/g, '_')} />
   if (!loading && !list) return <Redirect to="/404" />
   return (
     <Home>
-      {list && <ListInfo name={list.name} description={list.description} />}
+      {type === 'list' && (
+        <ListInfo name={list.name} description={list.description} />
+      )}
       <MentorList
         mentors={
           list.users?.map(user => ('user' in user ? user.user : user)) ?? []
