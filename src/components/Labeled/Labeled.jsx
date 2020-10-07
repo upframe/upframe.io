eb277b046a148d7b0e80666b11595e139fd9f34e
Mathis Bullinger <mathis@bullinger.dev>
2020-10-07 21:59:23 +0200

    feat: create spaces

--- src/components/Labeled/Labeled.jsx
+++ src/components/Labeled/Labeled.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default function Labeled({ label, action }) {
+export default function Labeled({ label, action, wrap = false }) {
+  const Wrap = wrap ? 'div' : React.Fragment
+
   return (
-    <>
+    <Wrap>
       <label htmlFor={label}>{label}</label>
       {React.cloneElement(action, { id: label })}
-    </>
+    </Wrap>
   )
 }
--- src/screens/tools/Spaces.tsx
+++ src/screens/tools/Spaces.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { gql, useQuery } from 'gql'
-import type { Spaces as SpacesQuery } from 'gql/types'
+import { gql, useQuery, useMutation } from 'gql'
+import { Title, Input, Labeled, Button } from 'components'
+import type {
+  Spaces as SpacesQuery,
+  CreateSpace as CreateSpaceMutation,
+  CreateSpaceVariables,
+} from 'gql/types'
+
+const SPACE = gql`
+  fragment ToolsSpace on Space {
+    id
+    name
+  }
+`
 
 const SPACES_QUERY = gql`
   query Spaces {
     spaces {
-      id
-      name
-      handle
+      ...ToolsSpace
     }
   }
+  ${SPACE}
+`
+
+const CREATE_SPACE = gql`
+  mutation CreateSpace($name: String!, $handle: String!) {
+    createSpace(name: $name, handle: $handle) {
+      ...ToolsSpace
+    }
+  }
+  ${SPACE}
 `
 
 export default function Spaces() {
@@ -18,6 +38,9 @@ export default function Spaces() {
 
   return (
     <S.Spaces>
+      <Title size={2}>Create Space</Title>
+      <CreateSpace />
+      <Title size={2}>Spaces</Title>
       <S.List>
         {spaces.map(({ id, name }) => (
           <li key={id}>{name}</li>
@@ -27,8 +50,93 @@ export default function Spaces() {
   )
 }
 
+function CreateSpace() {
+  const [name, setName] = useState('')
+  const [handle, setHandle] = useState('')
+  const [manualHandle, setManualHandle] = useState(false)
+
+  function reset() {
+    setName('')
+    setHandle('')
+    setManualHandle(false)
+  }
+
+  const [createSpace] = useMutation<CreateSpaceMutation, CreateSpaceVariables>(
+    CREATE_SPACE,
+    {
+      variables: { name, handle },
+      update(cache, { data }) {
+        const { spaces = [] } = cache.readQuery({ query: SPACES_QUERY }) ?? {}
+        cache.writeQuery({
+          query: SPACES_QUERY,
+          data: { spaces: [...spaces, data?.createSpace] },
+        })
+        reset()
+      },
+    }
+  )
+
+  useEffect(() => {
+    if (manualHandle) return
+    setHandle(name)
+  }, [name, manualHandle])
+
+  function create(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    createSpace()
+  }
+
+  return (
+    <S.Create onSubmit={create}>
+      <Labeled
+        wrap
+        label="name"
+        action={<Input value={name} onChange={setName} />}
+      />
+      <Labeled
+        wrap
+        label="handle"
+        action={
+          <Input
+            value={handle}
+            onChange={v => {
+              setManualHandle(true)
+              setHandle(v)
+            }}
+          />
+        }
+      />
+      <Button type="submit" accent>
+        create
+      </Button>
+    </S.Create>
+  )
+}
+
 const S = {
-  Spaces: styled.div``,
+  Spaces: styled.div`
+    padding-left: 5vw;
+    width: 100%;
+
+    & > *:first-child {
+      margin-top: 0;
+    }
+  `,
 
   List: styled.ul``,
+
+  Create: styled.form`
+    display: flex;
+    width: 100%;
+    align-items: flex-end;
+
+    & > *:not(:last-child) {
+      margin-right: 1rem;
+      flex-grow: 1;
+
+      input {
+        width: 100%;
+      }
+    }
+  `,
 }
